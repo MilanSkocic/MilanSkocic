e051c6764d5b431db47c6ac9b6d7daae70df7119
Milan Skocic <milan.skocic@gmail.com>
2025-06-25 15:43:17 +0200

    Create app.sh

---
+++ snippets/bash/app.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+# DEFINE
+PROGNAME="program"
+PROGVERSION="1.0"
+SHORTDESCRIPTION="Short description"
+HOMEPAGE="Home Page"
+LICENSE="MIT"
+MANSECTION="1"
+
+# MENU FUNCTIONS
+help () {
+    echo "NAME"
+    echo "    $PROGNAME($MANSECTION) - $SHORTDESCRIPTION"
+    echo ""
+
+    echo "SYNOPSIS"
+    echo "    $PROGNAME SUBCOMMAND [SUBCOMMAND_OPTIONS]" 
+    echo ""
+
+    echo "DESCRIPTION"
+    echo "   $PROGNAME is program that ..."
+    echo ""
+    
+    echo "SUBCOMMANDS"
+    echo "  + build    Compile code."
+    echo ""
+    
+    echo "SUBCOMMANDS_OPTIONS"
+    echo " -d, --directory PATH    Set the working directory"
+    echo ""
+
+    echo "EXAMPLES"
+    echo "  $PROGNAME SUBCOMMAND COMMAND"
+    
+    echo "SEE ALSO"
+    echo "  + The home page is ..."
+    echo ""
+
+}
+
+help_usage () {
+    echo "$PROGNAME:"
+    echo ""
+    echo "USAGE: $PROGNAME [SUBCOMMAND [SUBCOMMAND_OPTIONS]]|[--list|--help|--version]"
+}
+
+version () {
+    echo "Version:      $PROGVERSION"
+    echo "Program:      $PROGNAME"
+    echo "Description:  $SHORTDESCRIPTION"
+    echo "Home Page:    $HOMEPAGE"
+    echo "License:      $LICENSE"
+    echo "OS Type:      $OSTYPE"
+}
+
+
+case $1 in 
+    "--help"|"-h")
+        help
+        ;;
+    "--version"|"-v")
+        version
+        ;;
+    *)
+        help_usage
+        ;;
+esac
