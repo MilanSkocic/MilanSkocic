6944561cf8ad6ff4071bab5d94f0ed4241292e5e
Milan Skocic <milan.skocic@gmail.com>
2025-07-26 09:56:44 +0200

    Update wezterm config.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -4,17 +4,25 @@ local wezterm = require("wezterm")
 -- config
 local config = wezterm.config_builder()
 
+-- theme
 config.color_scheme = 'Catppuccin Mocha'
 
 
 -- font
 config.font_size = 14
+-- config.dpi=110
 config.font = wezterm.font("UbuntuMono Nerd Font Mono")
 config.adjust_window_size_when_changing_font_size = false
 
--- tabs
-config.hide_tab_bar_if_only_one_tab = false
 
+-- appearance
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE|TITLE"
+config.window_padding = {left=0, right=0, top=0, bottom=0}
+config.window_background_opacity=0.9
+
+
+-- key maps
 config.keys = {
   -- This will create a new split and run your default program inside it
   {
