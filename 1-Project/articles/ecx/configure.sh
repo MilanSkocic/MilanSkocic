76c908d4c8da58ea014b21ad25839b87e95528be
Milan Skocic <milan.skocic@gmail.com>
2025-10-05 07:35:53 +0200

    Project/article/ecx: Use common reference.bib file.

--- 1-Project/articles/ecx/configure.sh
+++ 1-Project/articles/ecx/configure.sh
@@ -8,7 +8,7 @@ url=../../../3-Resource/figures/png
 target=PEC-*.png
 cp -fv $url/$target ./src/figures/
 
-url=../../../3-Resource/references/
-target=electrochemistry.bib
+url=../../../3-Resource/references
+target=references.bib
 cp -fv $url/$target ./src/references.bib
 
