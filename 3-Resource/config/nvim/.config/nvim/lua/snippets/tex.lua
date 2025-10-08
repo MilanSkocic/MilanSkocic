4a6988b5a009ac5aee5807cabcbb8b9fb2a959df
Milan Skocic <milan.skocic@gmail.com>
2025-10-08 17:36:49 +0200

    config: update snippets for tex.

--- 3-Resource/config/nvim/.config/nvim/lua/snippets/tex.lua
+++ 3-Resource/config/nvim/.config/nvim/lua/snippets/tex.lua
@@ -39,12 +39,40 @@ return {
         { t("\\alpha") }
     ),
     s(
+        { trig = ";A", snippetType="autosnippet",  wordTrig=false },
+        { t("\\Alpha") }
+    ),
+    s(
         { trig = ";b", snippetType="autosnippet", wordTrig=false },
         { t("\\beta") }
     ),
     s(
+        { trig = ";B", snippetType="autosnippet", wordTrig=false },
+        { t("\\Beta") }
+    ),
+    s(
         { trig = ";g", snippetType="autosnippet", wordTrig=false },
         { t("\\gamma") }
+    ),
+    s(
+        { trig = ";G", snippetType="autosnippet", wordTrig=false },
+        { t("\\Gamma") }
+    ),
+    s(
+        { trig = ";d", snippetType="autosnippet", wordTrig=false },
+        { t("\\delta") }
+    ),
+    s(
+        { trig = ";D", snippetType="autosnippet", wordTrig=false },
+        { t("\\Delta") }
+    ),
+    s(
+        { trig = ";e", snippetType="autosnippet", wordTrig=false },
+        { t("\\epsilon") }
+    ),
+    s(
+        { trig = ";E", snippetType="autosnippet", wordTrig=false },
+        { t("\\Epsilon") }
     ),
 
     -- sections
