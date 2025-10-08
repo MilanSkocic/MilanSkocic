32534b68f4b15863f56fbdeb341748f0d03821f7
Milan Skocic <milan.skocic@gmail.com>
2025-10-08 09:06:18 +0200

    config: add snippets for tex.

--- 3-Resource/config/nvim/.config/nvim/lua/snippets/tex.lua
+++ 3-Resource/config/nvim/.config/nvim/lua/snippets/tex.lua
@@ -1,6 +1,45 @@
+local line_begin = require("luasnip.extras.expand_conditions").line_begin
+
 return {
         s(
-            { trig = ";a" snippetType="snippet", desc="alpha", wordTrig=false },
+            { trig = ";a", snippetType="autosnippet",  wordTrig=false },
             { t("\\alpha") }
-        )
+        ),
+        s(
+            { trig = ";b", snippetType="autosnippet", wordTrig=false },
+            { t("\\beta") }
+        ),
+        s(
+            { trig = ";g", snippetType="autosnippet", wordTrig=false },
+            { t("\\gamma") }
+        ),
+        s({trig = "h1", condition = line_begin },
+          fmta(
+            [[\section{<>}]],
+            { i(1) }
+          )
+        ),
+        s({trig = 'ff'},
+          fmta(
+            [[\frac{<>}{<>}]],
+            {
+              i(1),
+              i(2)
+            }
+          )
+        ),
+        s({trig="new", condition = line_begin},
+          fmta(
+            [[
+              \begin{<>}
+                  <>
+              \end{<>}
+            ]],
+            {
+              i(1),
+              i(2),
+              rep(1),
+            }
+          )
+        ),
 }
