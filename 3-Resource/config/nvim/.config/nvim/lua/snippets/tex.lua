28664743dfb783b73175502a66dbe60d40a7500d
Milan Skocic <milan.skocic@gmail.com>
2025-10-08 09:25:21 +0200

    config: update snippets for tex.

--- 3-Resource/config/nvim/.config/nvim/lua/snippets/tex.lua
+++ 3-Resource/config/nvim/.config/nvim/lua/snippets/tex.lua
@@ -1,45 +1,102 @@
 local line_begin = require("luasnip.extras.expand_conditions").line_begin
 
 return {
-        s(
-            { trig = ";a", snippetType="autosnippet",  wordTrig=false },
-            { t("\\alpha") }
-        ),
-        s(
-            { trig = ";b", snippetType="autosnippet", wordTrig=false },
-            { t("\\beta") }
-        ),
-        s(
-            { trig = ";g", snippetType="autosnippet", wordTrig=false },
-            { t("\\gamma") }
-        ),
-        s({trig = "h1", condition = line_begin },
-          fmta(
-            [[\section{<>}]],
-            { i(1) }
-          )
-        ),
-        s({trig = 'ff'},
-          fmta(
-            [[\frac{<>}{<>}]],
-            {
-              i(1),
-              i(2)
-            }
-          )
-        ),
-        s({trig="new", condition = line_begin},
-          fmta(
-            [[
-              \begin{<>}
-                  <>
-              \end{<>}
-            ]],
-            {
-              i(1),
-              i(2),
-              rep(1),
-            }
-          )
-        ),
+    -- formatting
+    s({trig = "it"},
+      fmta(
+        [[\textit{<>}]],
+        { i(1) }
+      )
+    ),
+    s({trig = "bf"},
+      fmta(
+        [[\textbf{<>}]],
+        { i(1) }
+      )
+    ),
+    s({trig = "em"},
+      fmta(
+        [[\emph{<>}]],
+        { i(1) }
+      )
+    ),
+    s({trig = "ul"},
+      fmta(
+        [[\underline{<>}]],
+        { i(1) }
+      )
+    ),
+    s({trig = "tc"},
+      fmta(
+        [[\textcolor{<>}{<>}]],
+        { i(1), i(2) }
+      )
+    ),
+
+    -- Greek letters
+    s(
+        { trig = ";a", snippetType="autosnippet",  wordTrig=false },
+        { t("\\alpha") }
+    ),
+    s(
+        { trig = ";b", snippetType="autosnippet", wordTrig=false },
+        { t("\\beta") }
+    ),
+    s(
+        { trig = ";g", snippetType="autosnippet", wordTrig=false },
+        { t("\\gamma") }
+    ),
+
+    -- sections
+    s({trig = "h1", condition = line_begin },
+      fmta(
+        [[\section{<>}]],
+        { i(1) }
+      )
+    ),
+
+    -- environments
+    s({trig = 'ff'},
+      fmta(
+        [[\frac{<>}{<>}]],
+        {
+          i(1),
+          i(2)
+        }
+      )
+    ),
+    s({trig = 'ii'},
+      fmta(
+        [[\item <> ]],
+        {
+          i(1),
+        }
+      )
+    ),
+    s({trig="new", condition = line_begin},
+      fmta(
+        [[
+          \begin{<>}
+              <>
+          \end{<>}
+        ]],
+        {
+          i(1),
+          i(2),
+          rep(1),
+        }
+      )
+    ),
+    s({trig="neweq", condition = line_begin},
+      fmta(
+        [[
+          \begin{equation}
+              <>
+          \end{equation}
+        ]],
+        {
+          i(1),
+        }
+      )
+    ),
 }
