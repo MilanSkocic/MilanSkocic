b472deba5eb909caf41c618c81068acfc708c836
Milan Skocic <milan.skocic@gmail.com>
2025-10-15 06:25:57 +0200

    [2-Area/snippets] Update bash with rsync.

--- 2-Area/snippets/bash/backup.sh
+++ 2-Area/snippets/bash/backup.sh
@@ -8,11 +8,11 @@ DATETIME="$(date '+%Y-%m-%d_%H:%M:%S')"
 BACKUP_PATH="${BACKUP_DIR}/${DATETIME}"
 LATEST_LINK="${BACKUP_DIR}/latest"
 
-if [[ ! -d $SOURCE_DIR ]]; then echo "Source dir $SRC does not exist."; exit 1; fi
-if [[ ! -d $BACKUP_DIR ]]; then echo "Source dir $DEST does not exist."; exit 1; fi
+if [[ ! -d $SOURCE_DIR ]]; then echo "Source dir $SOURCE_DIR does not exist."; exit 1; fi
+if [[ ! -d $BACKUP_DIR ]]; then echo "Backup dir $BACKUP_DIR does not exist."; exit 1; fi
 
 
-rsync -ar --delete --info=progress2 "${SOURCE_DIR}/" --link-dest "${LATEST_LINK}" --exclude=".cache" "${BACKUP_PATH}"
+rsync -aruh --delete --info=progress2 "${SOURCE_DIR}/" --link-dest "${LATEST_LINK}" --exclude=".cache" "${BACKUP_PATH}"
 
 rm -rf "${LATEST_LINK}"
 ln -s "${BACKUP_PATH}" "${LATEST_LINK}"
